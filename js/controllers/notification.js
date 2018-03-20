0d3da66a603c3c95d4399d00a5f55d438bdbae01
cw-leung <cleung@cubewise.com>
2018-03-20 16:35:35 +0800

    1. Added directive sumByKey.js to calculate subtotal by filter
    2. Added directive notification.js
    3. New feature input comment by task list style

---
+++ js/controllers/notification.js
@@ -0,0 +1,21 @@
+(function () {
+  var app = angular.module('app');
+  app.directive('notification', ['$log', '$timeout', function ($log, $timeout) {
+    return {
+      scope: {
+        show: '=',
+        type: '@',
+        message: '@',
+        timeout: '@'
+       },
+      template: '<div uib-alert class="alert alert-dismissible {{alertType}}"  ng-show="show" dismiss-on-timeout="{{timeout}}" close="closeAlert()">{{timeout}}{{message}}</div>',
+      link: function ($scope, $elements, $attributes, directiveCtrl, transclude) {
+        $scope.alertType = 'alert-' + ($scope.type || 'warning');
+        // TODO: timeout is not working in comment Modal
+        $scope.closeAlert = function () {
+          $scope.show = false;
+        };
+      }
+    };
+  }]);
+})();
--- js/controllers/timesheet_input_by_day.js
+++ js/controllers/timesheet_input_by_day.js
@@ -1,5 +1,5 @@
-app.controller('timesheet_input_by_dayCtrl', ['$scope', '$rootScope', '$log', '$tm1Ui', '$location', '$$helper',
-  function ($scope, $rootScope, $log, $tm1Ui, $location, $$helper) {
+app.controller('timesheet_input_by_dayCtrl', ['$scope', '$rootScope', '$log', '$tm1Ui', '$location', '$$helper', '$filter',
+  function ($scope, $rootScope, $log, $tm1Ui, $location, $$helper, $filter) {
     /*
      *     defaults.* are variables that are declared once and are changed in the page, otherwise known as constants in programming languages
      *     lists.* should be used to store any lists that are used with ng-repeat, i.e. tm1-ui-element-list
@@ -13,7 +13,7 @@ app.controller('timesheet_input_by_dayCtrl', ['$scope', '$rootScope', '$log', '$
       instance: 'dev',
       sysInfoCube: 'System Info',
       sysInfoMeasure: 'String',
-      max: 16
+      ratingMax: 16
     };
     $scope.selections = {
     };
@@ -39,6 +39,11 @@ app.controller('timesheet_input_by_dayCtrl', ['$scope', '$rootScope', '$log', '$
       is1Open: false,
       is2Open: false,
       overIcon: {}
+    };
+
+    $scope.commentModal = {
+      weekday: '',
+      project: []
     };
 
     $scope.mytime = "1:00";
@@ -68,8 +73,15 @@ app.controller('timesheet_input_by_dayCtrl', ['$scope', '$rootScope', '$log', '$
           });
       });
 
-      // for uib rating bar title
-      $scope.defaults.ratingTitle = _.range(8 / $scope.defaults.max, 9, 8 / $scope.defaults.max);
+      //for uib rating bar tooltips
+      $scope.defaults.ratingTitle = _.range(8 / $scope.defaults.ratingMax, 9, 8 / $scope.defaults.ratingMax);
+
+      //commentList from local storage
+      if ($rootScope.uiPrefs.commentList) {
+        $scope.values.comments = $rootScope.uiPrefs.commentList;
+      } else {
+        $scope.values.comments = [];
+      }
 
     }
 
@@ -83,7 +95,7 @@ app.controller('timesheet_input_by_dayCtrl', ['$scope', '$rootScope', '$log', '$
 
 
     $scope.hoveringOver = function (project, measure, value) {
-      value = value / ($scope.defaults.max / 8);
+      value = value / ($scope.defaults.ratingMax / 8);
 
       var overIconObj = {};
       overIconObj[measure] = true;
@@ -101,13 +113,80 @@ app.controller('timesheet_input_by_dayCtrl', ['$scope', '$rootScope', '$log', '$
     };
 
     $scope.updateHour = function (weekday, project, measure, value) {
-      value = value / ($scope.defaults.max / 8);
+      value = value / ($scope.defaults.ratingMax / 8);
       $tm1Ui.cellPut(value, $scope.defaults.instance, 'Timesheet', 'Actual', $scope.selections.Year, $scope.selections.Week, weekday, project, $scope.values.userName.Name, 'Local', measure).then(function () {
         $tm1Ui.dataRefresh();
       });
     };
 
+    $scope.showCommentModal = function (weekday, projectObj) {
+      $scope.commentModal.weekday = weekday;
+      $scope.commentModal.projectObj = projectObj;
+    };
+
+    $scope.closeCommentModal = function () {
+      $tm1Ui.dataRefresh();
+    }
+
+    $scope.addComment = function (commentObj) {
+      commentObj.weekday = $scope.commentModal.weekday;
+      commentObj.project = $scope.commentModal.projectObj.Index;
+
+      $scope.values.comments.push(commentObj);
+
+      //reset $scope.comment, $scope.commentHour
+      $scope.comment = '';
+      $scope.commentHour = null;
+    };
+
+    $scope.deleteComment = function (commentObj) {
+      commentObj.weekday = $scope.commentModal.weekday;
+      commentObj.project = $scope.commentModal.projectObj.Index;
+
+      var result = $filter('filter')($scope.values.comments, commentObj, true)[0];
+      if (result) {
+        var pos = $scope.values.comments.indexOf(result);
+        $scope.values.comments.splice(pos, 1);
+      }
+    };
+
+    $scope.submitComment = function () {
+      var commentObj = {};
+      var weekday = $scope.commentModal.weekday;
+      var projectObj = $scope.commentModal.projectObj;
+      var measure = 'Project Consulting Hours';
+
+      var results = $filter('filter')($scope.values.comments, { weekday: weekday, project: projectObj.Index }, true);
+      if (results) {
+        var total = 0;
+        var commentStr = '';
+        for (var i = 0; i < results.length; i++) {
+          total += parseInt(results[i].commentHour);
+          commentStr += results[i].comment + '(' + parseInt(results[i].commentHour) + 'h) ';
+        }
+        // console.log('total: ', total, ' str: ', commentStr);
+
+        $tm1Ui.cellPut(total, $scope.defaults.instance, 'Timesheet', 'Actual', $scope.selections.Year, $scope.selections.Week, weekday, projectObj.Name, $scope.values.userName.Name, 'Local', measure).then(function (data) {
+          if (!data.success) {
+            $log.error(data);
+          }
+        });
+        $tm1Ui.cellPut(commentStr, $scope.defaults.instance, 'Timesheet', 'Actual', $scope.selections.Year, $scope.selections.Week, weekday, projectObj.Name, $scope.values.userName.Name, 'Local', 'Comment').then(function (data) {
+          if (!data.success) {
+            $log.error(data);
+          }
+
+          // TODO: handle promise
+          $scope.showSuccess = true;
+        });
+
+      }
+    };
 
 
+
+    $scope.$watch("values.comments", function (newVal, oldVal) {
+      $rootScope.uiPrefs.commentList = $scope.values.comments;
+    }, true);
 
   }]);
